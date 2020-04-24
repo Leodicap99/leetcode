d4135721523540a5ca151688bb29b24123ab597d
Aditya Skumar <38405597+Leodicap99@users.noreply.github.com>
2020-04-24 19:06:00 +0530

    Update and rename 121. Best Time to Buy and Sell Stock.cpp to Dynamic Programming/121. Best Time to Buy and Sell Stock.cpp

--- Dynamic Programming/121. Best Time to Buy and Sell Stock.cpp
+++ Dynamic Programming/121. Best Time to Buy and Sell Stock.cpp
@@ -1,3 +1,4 @@
+/*
 What u want to keep doing in this problem is recording the maximum profit and recording the minimum element at the same time.
 say the array is [4,3,6,8,9].
 let my smallest element be INT_MAX
@@ -12,7 +13,7 @@ profit  smallest
   5        3
   6        3
   Thus 3 is the final answer.
-
+*/
       int maxProfit(vector<int>& prices) {
         int profit=0,smallest=INT_MAX;
         for(int i=0;i<prices.size();i++)
