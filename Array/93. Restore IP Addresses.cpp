b9073bfcee183e9fc72088510be84f9d862e60f7
Aditya Skumar <38405597+Leodicap99@users.noreply.github.com>
2020-06-19 19:11:27 +0530

    Create 93. Restore IP Addresses.cpp

---
+++ Array/93. Restore IP Addresses.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<string> restoreIpAddresses(string s) {
+        vector<string> ans;
+        string temp;
+        for(int a=1;a<=3;a++)
+            for(int b=1;b<=3;b++)
+                for(int c=1;c<=3;c++)
+                    for(int d=1;d<=3;d++)
+                    {
+                        if(a+b+c+d==s.length())
+                        {
+                            int A=stoi(s.substr(0,a));
+                            int B=stoi(s.substr(a,b));
+                            int C=stoi(s.substr(a+b,c));
+                            int D=stoi(s.substr(a+b+c,d));
+                            if(A<=255 && B<=255 && C<=255 && D<=255)
+                                if((temp=to_string(A)+'.'+to_string(B)+'.'+to_string(C)+'.'+to_string(D)).length()==s.length()+3)
+                                    ans.push_back(temp);
+                        }  
+                    }
+        return ans;
+    }
+};
