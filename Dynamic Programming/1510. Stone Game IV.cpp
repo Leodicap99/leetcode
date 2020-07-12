3e71834120c22aede1e3add1fc798a39a0e9f1a1
Aditya Skumar <38405597+Leodicap99@users.noreply.github.com>
2020-07-12 10:57:52 +0530

    Create 1510. Stone Game IV.cpp

---
+++ Dynamic Programming/1510. Stone Game IV.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool winnerSquareGame(int n) {
+        vector<int> dp(n+1,0);
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j*j<=i;j++)
+            {
+                if(!dp[i-j*j])
+                {
+                    dp[i]=true;
+                    break;
+                }
+            }
+        }
+        return dp[n];
+    }
+};
